c7d1de49b4b2254f2f19d6f76768becf3c8ce0d5
Joana Coll <jectango@gmail.com>
2024-11-08 13:26:37 -0300

    Added exception handling

--- Works/PatternFactory/src/main/java/org/example/Main.java
+++ Works/PatternFactory/src/main/java/org/example/Main.java
@@ -5,34 +5,38 @@ import org.example.model.Document;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("---- TEXT DOCUMENT --------");
-        Document textDocument = DocumentFactory.createDocument("Text Document");
-        textDocument.open();
-        textDocument.save();
-        textDocument.close();
+        try {
+            System.out.println("---- TEXT DOCUMENT --------");
+            Document textDocument = DocumentFactory.createDocument("Text Document");
+            textDocument.open();
+            textDocument.save();
+            textDocument.close();
 
-        System.out.println("\n---- SPREADSHEET DOCUMENT --------");
-        Document spreadsheetDocument = DocumentFactory.createDocument("Spreadsheet Document");
-        spreadsheetDocument.open();
-        spreadsheetDocument.save();
-        spreadsheetDocument.close();
+            System.out.println("\n---- SPREADSHEET DOCUMENT --------");
+            Document spreadsheetDocument = DocumentFactory.createDocument("Spreadsheet Document");
+            spreadsheetDocument.open();
+            spreadsheetDocument.save();
+            spreadsheetDocument.close();
 
-        System.out.println("\n---- PRESENTATION DOCUMENT --------");
-        Document presentationDocument = DocumentFactory.createDocument("Presentation Document");
-        presentationDocument.open();
-        presentationDocument.save();
-        presentationDocument.close();
+            System.out.println("\n---- PRESENTATION DOCUMENT --------");
+            Document presentationDocument = DocumentFactory.createDocument("Presentation Document");
+            presentationDocument.open();
+            presentationDocument.save();
+            presentationDocument.close();
 
-        System.out.println("\n---- PDF DOCUMENT --------");
-        Document pdfDocument = DocumentFactory.createDocument("PDF Document");
-        pdfDocument.open();
-        pdfDocument.save();
-        pdfDocument.close();
+            System.out.println("\n---- PDF DOCUMENT --------");
+            Document pdfDocument = DocumentFactory.createDocument("PDF Document");
+            pdfDocument.open();
+            pdfDocument.save();
+            pdfDocument.close();
 
-        System.out.println("\n---- IMAGE DOCUMENT --------");
-        Document imageDocument = DocumentFactory.createDocument("Image Document");
-        imageDocument.open();
-        imageDocument.save();
-        imageDocument.close();
+            System.out.println("\n---- IMAGE DOCUMENT --------");
+            Document imageDocument = DocumentFactory.createDocument("Image Document");
+            imageDocument.open();
+            imageDocument.save();
+            imageDocument.close();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
